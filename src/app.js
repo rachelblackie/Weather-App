e3b89cd0df28aa63694472f55c03d43709515716
rachelblackie <rachelannblackie@gmail.com>
2022-11-23 10:26:25 +0100

    work on dates

--- src/app.js
+++ src/app.js
@@ -31,7 +31,7 @@ function displayForecast() {
       `         
           <div class="weather-forecast" id="forecast">
               <div class="col-2">
-                <div class="forecast-date text">Wednesday</div>
+                <div class="forecast-date text">'${day}'</div>
                 <div class="forecast-icon">⛅</div>
                 <div class="forecast-temp text">10/20</div>
               </div>
